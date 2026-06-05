be5fe92e030d9c37c3cb23aaf429014cd5180c04
Gökmen Kaplan <gokmen.kaplan.9163@gmail.com>
2026-06-05 19:28:44 +0300

    Add media keys support to sway

---
+++ modules/sway/media.nix
@@ -0,0 +1,24 @@
+{
+  flake.modules.homeManager.sway =
+    { pkgs, lib, ... }:
+    {
+      wayland.windowManager.sway.extraConfig = ''
+        bindsym --locked {
+          XF86AudioPlay exec playerctl play-pause
+          XF86AudioStop exec playerctl stop
+        }
+      '';
+
+      wayland.windowManager.sway.config.keybindings = lib.mkOptionDefault {
+        XF86AudioForward = "exec playerctl position +10";
+        XF86AudioNext = "exec playerctl next";
+        XF86AudioPause = "exec playerctl pause";
+        XF86AudioPrev = "exec playerctl previous";
+        XF86AudioRewind = "exec playerctl position -10";
+      };
+
+      home.packages = with pkgs; [
+        playerctl
+      ];
+    };
+}

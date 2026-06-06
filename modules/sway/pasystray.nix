382e1b2dbaf504c0b603c361badebb7061b5c215
Gökmen Kaplan <gokmen.kaplan.9163@gmail.com>
2026-06-07 00:43:27 +0300

    Make pavucontrol windows float

--- modules/sway/pasystray.nix
+++ modules/sway/pasystray.nix
@@ -1,5 +1,12 @@
 {
   den.aspects.sway.homeManager = {
     services.pasystray.enable = true;
+
+    wayland.windowManager.sway.extraConfig = ''
+      for_window [app_id="pavucontrol"] {
+          floating enable
+          move position center
+      }
+    '';
   };
 }

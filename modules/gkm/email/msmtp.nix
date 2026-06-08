f82abdf8bed0c0edb2edc1e0e471c1a1695edbb7
Gökmen Kaplan <gokmen.kaplan.9163@gmail.com>
2026-06-08 19:09:14 +0300

    Enable msmtp for the gkm aspect

---
+++ modules/gkm/email/msmtp.nix
@@ -0,0 +1,8 @@
+{
+  den.aspects.gkm.homeManager = {
+    programs.msmtp.enable = true;
+
+    accounts.email.accounts.personal.msmtp.enable = true;
+    accounts.email.accounts.school.msmtp.enable = true;
+  };
+}

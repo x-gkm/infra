7e4a59b1aeef8328c169774fe2a400491d808828
Gökmen Kaplan <gokmen.kaplan.9163@gmail.com>
2026-06-08 19:06:23 +0300

    Enable allowUnfree for nix-shell etc.

--- modules/base/default.nix
+++ modules/base/default.nix
@@ -1,6 +1,15 @@
 {
-  den.aspects.base.nixos = {
-    nix.settings.experimental-features = "nix-command flakes";
-    nixpkgs.config.allowUnfree = true;
+  den.aspects.base = {
+    nixos = {
+      nix.settings.experimental-features = "nix-command flakes";
+      nixpkgs.config.allowUnfree = true;
+    };
+    homeManager = {
+      xdg.configFile."nixpkgs/config.nix".text = ''
+        {
+          allowUnfree = true;
+        }
+      '';
+    };
   };
 }

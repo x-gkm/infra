b4285cf66e8cc2ed5df70643e991644ca1e4b8b2
Gökmen Kaplan <gokmen.kaplan.9163@gmail.com>
2026-06-11 17:27:20 +0300

    Pass "--unsupported-gpu" to sway

--- modules/hosts/gkm-laptop/nvidia.nix
+++ modules/hosts/gkm-laptop/nvidia.nix
@@ -1,7 +1,15 @@
 {
-  den.aspects.gkm-laptop.nixos = {
-    hardware.graphics.enable = true;
-    services.xserver.videoDrivers = [ "nvidia" ];
-    hardware.nvidia.open = true;
+  den.aspects.gkm-laptop = {
+    nixos = {
+      hardware.graphics.enable = true;
+      services.xserver.videoDrivers = [ "nvidia" ];
+      hardware.nvidia.open = true;
+
+      programs.sway.extraOptions = [ "--unsupported-gpu" ];
+    };
+
+    homeManager = {
+      wayland.windowManager.sway.extraOptions = [ "--unsupported-gpu" ];
+    };
   };
 }
